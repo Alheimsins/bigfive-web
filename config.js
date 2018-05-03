ba1e1913f16be3fecaace44344ce8f83240ad719
Jonas Enge <jonas.enge@t-fk.no>
2018-05-04 00:55:52 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ module.exports = {
   JWT_SECRET: process.env.JWT_SECRET || 'Gibberish, jibberish, jibber-jabber and gobbledygook',
   DB_CONNECTION: process.env.DB_CONNECTION || 'username:password@localhost/bigfive',
   DB_COLLECTION: process.env.DB_COLLECTION || 'results',
-  URL: process.env.URL || 'http://bigfive-test.com'
+  URL: process.env.URL || 'http://localhost:3000'
 }
