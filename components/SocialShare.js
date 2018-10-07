a6e92c4c35c4ae9027b3fc78a7500ade578be0e6
Jonas Enge <jonas.enge@gmail.com>
2018-10-07 18:31:00 +0200

    Adds link copy

--- components/SocialShare.js
+++ components/SocialShare.js
@@ -1,11 +1,11 @@
 import { FaFacebook, FaTwitter, FaGooglePlus, FaLink } from 'react-icons/fa'
 import { CopyToClipboard } from 'react-copy-to-clipboard'
 
+// TODO: Notification when clicked copy text link
 export default ({ url }) => (
   <p className='share'>
         Share on{' '}
-    // TODO: Notification when clicked copy text link
-    <CopyToClipboard text={url}><a style={{ cursor: 'pointer' }}><FaLink /></a></CopyToClipboard>{' '}
+    <CopyToClipboard text={url}><a title='Copy to clipboard' style={{ cursor: 'pointer' }}><FaLink /></a></CopyToClipboard>{' '}
     <a href={`https://www.facebook.com/sharer/sharer.php?u=${url}`} title='Share on facebook'><FaFacebook /></a>{' '}
     <a href={`https://twitter.com/home?status=${url}`} title='Share on twitter'><FaTwitter />{' '}</a>
     <a href={`https://plus.google.com/share?url=${url}`} title='Share on google plus'><FaGooglePlus /></a>
