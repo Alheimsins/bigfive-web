535f4e12ab1cd83dd3fdbef0756c559fd166db9a
Jonas Enge <jonas.enge@gmail.com>
2018-10-10 14:00:42 +0200

    Adds copy notification

--- components/SocialShare.js
+++ components/SocialShare.js
@@ -1,23 +1,49 @@
+import { Component } from 'react'
 import { FaFacebook, FaTwitter, FaGooglePlus, FaLink } from 'react-icons/fa'
 import { CopyToClipboard } from 'react-copy-to-clipboard'
 
-// TODO: Notification when clicked copy text link
-export default ({ url }) => (
-  <p className='share'>
-        Share on{' '}
-    <CopyToClipboard text={url}><a title='Copy to clipboard' style={{ cursor: 'pointer' }}><FaLink /></a></CopyToClipboard>{' '}
-    <a href={`https://www.facebook.com/sharer/sharer.php?u=${url}`} title='Share on facebook'><FaFacebook /></a>{' '}
-    <a href={`https://twitter.com/home?status=${url}`} title='Share on twitter'><FaTwitter />{' '}</a>
-    <a href={`https://plus.google.com/share?url=${url}`} title='Share on google plus'><FaGooglePlus /></a>
-    <style jsx>
-      {`
-        .share {
-          text-align: right;
-        }
-        .share a {
-          color: black;
-        }
-      `}
-    </style>
-  </p>
-)
+export default class extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {}
+    this.showCopied = this.showCopied.bind(this)
+  }
+
+  showCopied () {
+    this.setState({ copied: true })
+    setTimeout(() => { this.setState({ copied: false }) }, 1000)
+  }
+
+  render () {
+    const { url } = this.props
+    return (
+      <p className='share'>
+            Share on{' '}
+        <CopyToClipboard text={url} onCopy={() => this.showCopied()}>
+          <a title='Copy to clipboard' style={{ cursor: 'pointer' }}>
+            { this.state.copied && <i className='copy-text'>Link copied</i> }
+            <FaLink />
+          </a>
+        </CopyToClipboard>{' '}
+        <a href={`https://www.facebook.com/sharer/sharer.php?u=${url}`} title='Share on facebook'><FaFacebook /></a>{' '}
+        <a href={`https://twitter.com/home?status=${url}`} title='Share on twitter'><FaTwitter />{' '}</a>
+        <a href={`https://plus.google.com/share?url=${url}`} title='Share on google plus'><FaGooglePlus /></a>
+        <style jsx>
+          {`
+            .copy-text {
+              background-color: #bd10e0;
+              color: white;
+              position: absolute;
+            }
+            .share {
+              text-align: right;
+            }
+            .share a {
+              color: black;
+            }
+          `}
+        </style>
+      </p>
+    )
+  }
+}
