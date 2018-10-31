7f7233fcde4d13a2d90be87bc7000c4035d6c7b9
Jonas Enge <jonas.enge@gmail.com>
2018-10-31 22:29:40 +0100

    Remove spring

--- components/SocialShare.js
+++ components/SocialShare.js
@@ -1,22 +1,6 @@
 import { Component } from 'react'
 import { FaFacebook, FaTwitter, FaGooglePlus, FaLink } from 'react-icons/fa'
 import { CopyToClipboard } from 'react-copy-to-clipboard'
-import { Transition, animated } from 'react-spring'
-
-const Copied = styles => (
-  <animated.i
-    style={{
-      backgroundColor: 'rgb(255, 0, 128)',
-      padding: '2px 10px',
-      color: 'white',
-      position: 'absolute',
-      borderRadius: '5px',
-      ...styles
-    }}
-  >
-    Link copied
-  </animated.i>
-)
 
 export default class extends Component {
   constructor (props) {
@@ -36,14 +20,14 @@ export default class extends Component {
       Share results  {' '}
       <CopyToClipboard text={url} onCopy={() => this.showCopied()}>
         <a title='Copy to clipboard' style={{ cursor: 'pointer' }}>
-          <Transition native from={{ opacity: 0, transform: 'translateX(10px)' }} enter={{ opacity: 1, transform: 'translateX(0)' }} leave={{ opacity: 0, transform: 'translateX(10px)' }}>
-            {this.state.copied && Copied}
-          </Transition>
+          { this.state.copied && <i className='copy-text'>Link copied</i> }
           <FaLink />
         </a>
-      </CopyToClipboard> <a href={`https://www.facebook.com/sharer/sharer.php?u=${url}`} title='Share on facebook'>
+      </CopyToClipboard>{' '}
+      <a href={`https://www.facebook.com/sharer/sharer.php?u=${url}`} title='Share on facebook'>
         <FaFacebook />
-      </a> <a href={`https://twitter.com/home?status=${url}`} title='Share on twitter'>
+      </a>
+      <a href={`https://twitter.com/home?status=${url}`} title='Share on twitter'>
         <FaTwitter />{' '}
       </a>
       <a href={`https://plus.google.com/share?url=${url}`} title='Share on google plus'>
@@ -53,6 +37,13 @@ export default class extends Component {
         {`
             .share {
               text-align: right;
+            }
+
+            .copy-text {
+              background-color: #bd10e0;
+              color: white;
+              position: absolute;
+              border-radius: 5px;
             }
 
             .share-text {
