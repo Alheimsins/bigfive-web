a8a8ae9ed04888591d8cb05c9bbe51fad9020792
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-01-19 21:14:31 +0100

    Changes locales model

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import { Select, Button, Field } from '../components/alheimsins'
 import { FaExternalLinkAlt } from 'react-icons/fa'
 import ISO6391 from 'iso-639-1'
 import { getInfo } from '@alheimsins/b5-johnson-120-ipip-neo-pi-r'
-import withI18next from '../hoc/withI18next'
 
 const { languages } = getInfo()
 const languageList = languages.map(code => ({ code, name: ISO6391.getName(code) }))
@@ -14,34 +13,35 @@ const startTest = e => {
   Router.pushRoute(`/test/${lang}`)
 }
 
-const Home = ({ countryCode, t }) => {
+const Home = ({ countryCode }) => {
   const defaultLanguage = languages.includes(countryCode) ? countryCode : 'en'
+  const translation = require('../locales/en/translations.json')
   return (
     <div>
       <div style={{ paddingBottom: '20px', borderBottom: '1px solid rgb(234, 234, 234)' }}>
-        <h1>{t('home:title')}</h1>
+        <h1>{translation.home.title}</h1>
       </div>
       <div style={{ textAlign: 'left', margin: 'auto', fontSize: '14px', width: '100%' }}>
-        <p>{t('home:paragraphs:first')}</p>
+        <p>{translation.home.paragraphs.first}</p>
         <p>
-          {t('home:paragraphs:test_evaluation')} <a href='http://ipip.ori.org' rel='noopener noreferrer' target='_blank'>ipip.ori.org</a>
-          <FaExternalLinkAlt size='8' style={{ verticalAlign: 'top' }} />, {t('home:paragraphs:inventory_is_from')} <i>{t('home:paragraphs:name_inventor')}</i>.
+          {translation.home.paragraphs.test_evaluation} <a href='http://ipip.ori.org' rel='noopener noreferrer' target='_blank'>ipip.ori.org</a>
+          <FaExternalLinkAlt size='8' style={{ verticalAlign: 'top' }} />, {translation.home.paragraphs.inventory_is_from} <i>{translation.home.paragraphs.name_inventor}</i>.
         </p>
-        <p> {t('home:paragraphs:the_following')} <b>{t('home:paragraphs:10_minutes')}</b> {t('home:paragraphs:to_complete')}</p>
-        <p>{t('home:paragraphs:after_submitted')} <i>{t('home:paragraphs:openess')}</i></p>
-        <p>{t('home:paragraphs:see_an')} <Link route='/result/58a70606a835c400c8b38e84'><a> {t('common:example')}</a></Link> {t('home:paragraphs:of_the_report')}.</p>
-        <h2>{t('common:tips')}</h2>
-        <p>- {t('home:paragraphs:tip1')}</p>
-        <p>- {t('home:paragraphs:tip2')}</p>
-        <p>- {t('home:paragraphs:tip3')}</p>
-        <h2>{t('common:language')}</h2>
+        <p> {translation.home.paragraphs.the_following} <b>{translation.home.paragraphs['10_minutes']}</b> {translation.home.paragraphs.to_complete}</p>
+        <p>{translation.home.paragraphs.after_submitted} <i>{translation.home.paragraphs.openess}</i></p>
+        <p>{translation.home.paragraphs.see_an} <Link route='/result/58a70606a835c400c8b38e84'><a> {translation.common.example}</a></Link> {translation.home.paragraphs.of_the_report}.</p>
+        <h2>{translation.common.tips}</h2>
+        <p>- {translation.home.paragraphs.tip1}</p>
+        <p>- {translation.home.paragraphs.tip2}</p>
+        <p>- {translation.home.paragraphs.tip3}</p>
+        <h2>{translation.common.language}</h2>
         <form onSubmit={startTest}>
-          <p>{t('home:paragraphs:select_language')}</p>
-          <Field name={t('common:language')}>
+          <p>{translation.home.paragraphs.select_language}</p>
+          <Field name={translation.common.language}>
             <Select name='languages' defaultValue={defaultLanguage} options={languageList} />
           </Field>
           <div style={{ textAlign: 'center', paddingTop: '20px' }}>
-            <Button value={t('home:paragraphs:start_test')} type='submit' background='rgb(255, 0, 128)' border='1px solid rgb(255, 0, 128)' />
+            <Button value={translation.home.paragraphs.start_test} type='submit' background='rgb(255, 0, 128)' border='1px solid rgb(255, 0, 128)' />
           </div>
         </form>
       </div>
@@ -49,4 +49,4 @@ const Home = ({ countryCode, t }) => {
   )
 }
 
-export default withI18next(['home', 'common'])(Home)
+export default Home
