b19248f660ff8d2bbf983e0e4c3e61e280bea93f
Peter <peter@nearprotocol.com>
2021-05-21 13:41:32 -0700

    add new text

--- components/RadarChart.js
+++ components/RadarChart.js
@@ -1,4 +1,4 @@
-import { Radar } from 'react-chartjs-2';
+import { Radar } from 'react-chartjs-2'
 
 const options = {
   legend: {
@@ -11,55 +11,55 @@ const options = {
   scale: {
     reverse: false,
     gridLines: {
-      circular: true,
+      circular: true
     },
     ticks: {
       beginAtZero: true
     }
   }
 }
-const data = {
-  labels: [
-    "Openness",
-    "Conscientiousness",
-    "Extraversion",
-    "Agreeableness",
-    "Neuroticism"
-  ],
-  datasets: [
-    {
-      label: 'My Personality',
-      backgroundColor: "rgba(139,0,213,0.4)",
-      pointBackgroundColor: "rgba(220,220,220,1)",
-      data: [
-        0.5,
-        0.8,
-        0.3,
-        0.5,
-        0.3
-      ]
-    }
-  ]
-}
+// const data = {
+//   labels: [
+//     "Openness",
+//     "Conscientiousness",
+//     "Extraversion",
+//     "Agreeableness",
+//     "Neuroticism"
+//   ],
+//   datasets: [
+//     {
+//       label: 'My Personality',
+//       backgroundColor: "rgba(139,0,213,0.4)",
+//       pointBackgroundColor: "rgba(220,220,220,1)",
+//       data: [
+//         0.5,
+//         0.8,
+//         0.3,
+//         0.5,
+//         0.3
+//       ]
+//     }
+//   ]
+// }
 
 const processData = props => {
   console.log(props)
   const title = props.title
   const labels = props.data.map(facet => {
     return facet.title
-  });
+  })
   const dataset = props.data.map(facet => {
-    return facet.score/(5*facet.count)
-  });
-  var r = () => Math.random() * 256 >> 0;
-  var color = `rgb(${r()}, ${r()}, ${r()})`;
+    return facet.score / (5 * facet.count)
+  })
+  var r = () => Math.random() * 256 >> 0
+  var color = `rgb(${r()}, ${r()}, ${r()})`
   return {
     labels: labels,
     datasets: [
       {
         label: title,
         backgroundColor: color,
-        pointBackgroundColor: "rgba(220,220,220,1)",
+        pointBackgroundColor: 'rgba(220,220,220,1)',
         data: dataset
       }
     ]
@@ -69,12 +69,13 @@ const processData = props => {
 const RadarChart = (props) => {
   return (
     <div>
-      <Radar 
+      <Radar
         width={20}
         height={20}
         data={processData(props)}
-        options={options} />
+        options={options}
+      />
     </div>
   )
 }
-export default RadarChart;
+export default RadarChart
